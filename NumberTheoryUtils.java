d3213d2e867270d3373cce8541e8028f19e4042c
Anshum Gupta <anshum_it_is@MacBook-Pro.local>
2020-08-17 21:48:50 +0530

    Number theory factorial & inverse facts with power added

--- NumberTheoryUtils.java
+++ NumberTheoryUtils.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
 
 public class NumberTheoryUtils {
+	private long MOD = (long)(1e9 + 7);
+	long[]fact, inverseFact;
+	boolean factsMade = false;
 	public long gcd(long a, long b) {
 		if(b == 0)return a;
 		return gcd(b, a%b);
@@ -43,4 +46,42 @@ public class NumberTheoryUtils {
 		}
 		return isPrime;
 	}
+	public void setMod(long MOD) {
+		this.MOD = MOD;
+	}
+	public long powerModulo(long base, long exponent) {
+		if(exponent == 0)return 1L;
+		long tempValue = powerModulo(base, exponent / 2L);
+		long res = (tempValue * tempValue) % MOD;
+		if(exponent % 2 != 0)res = (res * base) % MOD;
+		return res;
+	}
+	public long inverseModulo(long value) {
+		return this.powerModulo(value, MOD - 2L);
+	}
+	public long nCr(int n, int r) {
+		if(n < r || n < 0 || r < 0)return 0L;
+		return (((fact[n] * inverseFact[n-r]) % MOD) * inverseFact[r]) % MOD;
+	}
+	
+	public long[]getFactorialsModulo(int maxLength){
+		this.fact = new long[maxLength];
+		fact[0] = 1L;
+		for(int i=1; i<maxLength; i++) {
+			fact[i] = (fact[i-1] * i) % MOD;
+		}
+		this.factsMade = true;
+		return fact;
+	}
+	public long[]getInverseFactorialsModulo(int maxLength){
+		this.inverseFact = new long[maxLength];
+		if(!this.factsMade) {
+			this.getFactorialsModulo(maxLength);
+		}
+		for(int i=0; i<maxLength; i++) {
+			inverseFact[i] = this.inverseModulo(fact[i]);
+		}
+		return inverseFact;
+	}
+
 }
